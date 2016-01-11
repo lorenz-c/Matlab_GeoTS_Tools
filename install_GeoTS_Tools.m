170302c88a5b90e86101f230b13a6ff0269842c9
lorenz-c <Christof.Lorenz@kit.edu>
2016-01-11 13:54:21 +0100

    Added install file

---
+++ install_GeoTS_Tools.m
@@ -0,0 +1,10 @@
+tlbx_root = '/Users/lorenz-c/bin/Matlab_GeoTS_Tools/';
+
+disp(['Adding directory ', tlbx_root, 'Plot_Tools'])
+addpath([tlbx_root, 'Plot_Tools']);
+disp(['Adding directory ', tlbx_root, 'Read_Write_Tools'])
+addpath([tlbx_root, 'Read_Write_Tools']);
+disp(['Adding directory ', tlbx_root, 'TS_Base'])
+addpath([tlbx_root, 'TS_Base']);
+disp(['Adding directory ', tlbx_root, 'TS_Tools'])
+addpath([tlbx_root, 'TS_Tools']);
