ee2457438bd4fd6d8a9f2e525c476f023d506823
lorenz-c <Christof.Lorenz@kit.edu>
2018-02-14 11:04:56 +0100

    Added new functions: create_3d_netcdf LeanInterp bias_correction_mtrx
    Updates to drought_indices

---
+++ Read_Write_Tools/create_3d_netcdf.m
@@ -0,0 +1,89 @@
+function [] = create_3d_netcdf(fnme, dtainfo, varnme, varlong, varunits, times, lat, lon, time_units, chnks)   
+    
+
+    glbl_Atts = fieldnames(dtainfo);
+
+
+
+
+    if nargin < 7
+    	chnks = []; 
+    end
+    	
+    ntimes = length(times);
+
+    
+    if size(lat, 2) > 1
+        lat = lat(:, 1);
+    end
+    
+    if size(lon, 2) > 1
+        lon = lon(1, :)';
+    end
+    
+    nlat   = length(lat);
+    nlon   = length(lon);
+        
+    % Get the ID of the global attributes
+    glob_id = netcdf.getConstant('NC_GLOBAL');
+
+    % Set the parameters for NETCDF4-classic 
+    cmode   = netcdf.getConstant('NETCDF4');
+    cmode   = bitor(cmode,netcdf.getConstant('CLASSIC_MODEL'));
+
+    ncid        = netcdf.create(fnme, cmode);
+    
+    for i = 1:length(glbl_Atts)
+        netcdf.putAtt(ncid, glob_id, glbl_Atts{i, :}, dtainfo.(glbl_Atts{i}));
+    end
+
+    
+    time_dim_id = netcdf.defDim(ncid, 'time', ntimes);
+    lat_dim_id  = netcdf.defDim(ncid, 'lat', nlat);
+    lon_dim_id  = netcdf.defDim(ncid, 'lon', nlon);
+    
+    time_id     = netcdf.defVar(ncid, 'time', 'NC_FLOAT', time_dim_id);
+    lat_id      = netcdf.defVar(ncid, 'lat', 'NC_FLOAT', lat_dim_id);
+    lon_id      = netcdf.defVar(ncid, 'lon', 'NC_FLOAT', lon_dim_id);
+    
+    netcdf.putAtt(ncid, time_id, 'units', time_units);
+    netcdf.putAtt(ncid, lat_id, 'units', 'degrees_north');
+    netcdf.putAtt(ncid, lon_id, 'units', 'degrees_east');
+    
+    netcdf.putAtt(ncid, lat_id, 'standard_name', 'latitude');
+    netcdf.putAtt(ncid, lon_id, 'standard_name', 'longitude');
+    netcdf.putAtt(ncid, time_id, 'standard_name', 'time');
+    netcdf.putAtt(ncid, time_id, 'calendar', 'proleptic_gregorian');
+    
+    var_dims    = [lon_dim_id, lat_dim_id, time_dim_id];
+    
+    if iscell(varnme)
+        for i = 1:length(varnme)
+            var_id      = netcdf.defVar(ncid, varnme{i}, 'NC_FLOAT', var_dims);
+        %    netcdf.defVarFill(ncid, var_id, false, NaN);
+            
+            if ~isempty(chnks)
+                netcdf.defVarDeflate(ncid, var_id, false, false, 6);
+                netcdf.defVarChunking(ncid,  var_id, 'CHUNKED', chnks);
+            end
+        end
+    else
+        var_id = netcdf.defVar(ncid, varnme, 'NC_FLOAT', var_dims);
+        
+        netcdf.putAtt(ncid, var_id, 'long_name', varlong);
+        netcdf.putAtt(ncid, var_id, 'units', varunits);
+        
+        netcdf.defVarFill(ncid, var_id, false, NaN);
+         if ~isempty(chnks)
+                netcdf.defVarDeflate(ncid, var_id, false, false, 6);
+                netcdf.defVarChunking(ncid,  var_id, 'CHUNKED', chnks);
+         end
+    end
+
+    
+    netcdf.endDef(ncid)
+
+    ncwrite(fnme, 'time', single(times));
+    ncwrite(fnme, 'lat', single(lat));
+    ncwrite(fnme, 'lon', single(lon));      
+end
--- TS_Tools/drought_indices.m
+++ TS_Tools/drought_indices.m
@@ -1,20 +1,44 @@
 function out = drought_indices(inpt, scale, type)
 
+nts = size(inpt, 1);
+out = NaN(size(inpt));
+
 if strcmp(type, 'SPI_daily')
     
 elseif strcmp(type, 'SPI_param_monthly')
     % Fit a gamma distribution to the input data
-    pd = fitdist(inpt, 'Gamma');
+    %pd = fitdist(inpt, 'Gamma');
 elseif strcmp(type, 'SPI_emp_monthly')
-    tmp = NaN(length(inpt) - scale + 1, scale);
     
+    accum = zeros(nts - scale + 1, size(inpt, 2), size(inpt, 3));
+    
+    % Create the accumulated precipitation
     for i = 1:scale
-        tmp(:, i) = inpt(i:end-scale + i);
+        accum = accum + inpt(i : nts - scale + i, :, :);
     end
-    
-    % Compute seasonal sums
-    ssnl_sm = sum(tmp, 2);
-    
+
+    p_trafo = zeros(size(accum));
     
     
+    % Now, let's fit an empirical distribution
+    % TBA: Right now, it is assumed that the first month is January!
+    for i = 1:12
+        % Get the precipitation of the current month
+        mnthly_p = accum(i:12:end, :, :);
+        dst      = zeros(size(mnthly_p));
+        % Loop over each included timestep
+        for j = 1:size(mnthly_p, 1)
+            % Substract the actual month from the remaining months
+            delta = bsxfun(@minus, mnthly_p, mnthly_p(j, :, :));
+            % ...and count the number of elements which are <= 0 --> eCDF
+            dst(j, :, :) = sum(delta <= 0);
+        end
+        % Compute the marginal probability of precipitation using the
+        % empirical Gringorten plotting position
+        dst = (dst - 0.44)/(size(mnthly_p, 1) + 0.12);
+        p_trafo(i:12:end, :, :) = dst;
+    end
+    % Compute the SPIs using the inverse standard normal distribution
+    out(scale : end, :, :) = norminv(p_trafo);
+  
 end
